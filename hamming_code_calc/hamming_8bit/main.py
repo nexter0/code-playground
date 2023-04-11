faf33620855e28d0f75b979c80a8debd11020c5b
Nikodem <166309@student.uwm.edu.pl>
2023-04-11 19:35:05 +0200

    code clarifications

--- hamming_code_calc/hamming_8bit/main.py
+++ hamming_code_calc/hamming_8bit/main.py
@@ -4,8 +4,9 @@
 # Method according to UWM        #
 # lecturers.                     #
 ##################################
+from typing import List
 
-n = -1
+n: int = -1
 while n < 0 or n > 255:
     print("This calculator is designed for 8-bit numbers.\n\n")
     n = int(input('Enter a number (0 - 255): '))
@@ -19,22 +20,22 @@ while len(n_bin) < 8:
     n_bin = '0' + n_bin
 
 n_bin = n_bin[:4] + " " + n_bin[4:]
-n_bin_arr = [int(x) for x in n_bin if x.isdigit()]
+n_bin_arr: List[int] = [int(x) for x in n_bin if x.isdigit()]
 m = n_bin_arr[::-1]
 print(f'Your number in binary: {n_bin}')
 
 # Calculate check bits
-c0 = m[0] ^ m[1] ^ m[3] ^ m[4] ^ m[6]
-c1 = m[0] ^ m[2] ^ m[3] ^ m[5] ^ m[6]
-c2 = m[1] ^ m[2] ^ m[3] ^ m[7]
-c3 = m[4] ^ m[5] ^ m[6] ^ m[7]
+c0: int = m[0] ^ m[1] ^ m[3] ^ m[4] ^ m[6]
+c1: int = m[0] ^ m[2] ^ m[3] ^ m[5] ^ m[6]
+c2: int = m[1] ^ m[2] ^ m[3] ^ m[7]
+c3: int = m[4] ^ m[5] ^ m[6] ^ m[7]
 
 # Display check bits
 print(f'Check bits: ')
 print(f'C0: {c0}\nC1: {c1}\nC2: {c2}\nC3: {c3}')
 
 # Form, format & display Hamming code
-hamming_formatted = f'{c0}{c1}{m[0]}{c2} {m[1]}{m[2]}{m[3]}{c3} {m[4]}{m[5]}{m[6]}{m[7]}'
-hamming_no_space = f'{c0}{c1}{m[0]}{c2}{m[1]}{m[2]}{m[3]}{c3}{m[4]}{m[5]}{m[6]}{m[7]}'
+hamming_formatted: str = f'{c0}{c1}{m[0]}{c2} {m[1]}{m[2]}{m[3]}{c3} {m[4]}{m[5]}{m[6]}{m[7]}'
+hamming_no_spaces: str = f'{c0}{c1}{m[0]}{c2}{m[1]}{m[2]}{m[3]}{c3}{m[4]}{m[5]}{m[6]}{m[7]}'
 print(f'Hamming code: {hamming_formatted}')
-print(f'Hamming code in decimal: {int(hamming_no_space, 2)}')
+print(f'Hamming code in decimal: {int(hamming_no_spaces, 2)}')
--- hamming_code_calc/hamming_8bit_checker/main.py
+++ hamming_code_calc/hamming_8bit_checker/main.py
@@ -4,8 +4,9 @@
 # Method according to UWM     #
 # lecturers.                  #
 ###############################
+from typing import List
 
-n = -1
+n: int = -1
 while n < 0 or n > 4095:
     print("This calculator is designed for 12-bit codes.\n")
     n = int(input('Enter a number (0 - 4095): '))
@@ -22,20 +23,20 @@ while len(n_bin) < 12:
 n_bin_cpy = n_bin
 
 # Extract information and check bits from the code
-m = [n_bin[2], n_bin[4], n_bin[5], n_bin[6], n_bin[8], n_bin[9], n_bin[10], n_bin[11]]
-m_int = [int(x) for x in m]
-c_old = [n_bin[0], n_bin[1], n_bin[3], n_bin[7]]
-c_old = [int(x) for x in c_old]
+m: List[str] = [n_bin[2], n_bin[4], n_bin[5], n_bin[6], n_bin[8], n_bin[9], n_bin[10], n_bin[11]]
+m_int: List[int] = [int(x) for x in m]
+c_old: List[str] = [n_bin[0], n_bin[1], n_bin[3], n_bin[7]]
+c_old: List[int] = [int(x) for x in c_old]
 
 # Continue formatting and display given number in binary
 n_bin = n_bin[:4] + " " + n_bin[4:8] + " " + n_bin[8:12]
 print(f'Your number in binary: {n_bin}')
 
 # Calculate new check bits
-c0 = m_int[0] ^ m_int[1] ^ m_int[3] ^ m_int[4] ^ m_int[6]
-c1 = m_int[0] ^ m_int[2] ^ m_int[3] ^ m_int[5] ^ m_int[6]
-c2 = m_int[1] ^ m_int[2] ^ m_int[3] ^ m_int[7]
-c3 = m_int[4] ^ m_int[5] ^ m_int[6] ^ m_int[7]
+c0: int = m_int[0] ^ m_int[1] ^ m_int[3] ^ m_int[4] ^ m_int[6]
+c1: int = m_int[0] ^ m_int[2] ^ m_int[3] ^ m_int[5] ^ m_int[6]
+c2: int = m_int[1] ^ m_int[2] ^ m_int[3] ^ m_int[7]
+c3: int = m_int[4] ^ m_int[5] ^ m_int[6] ^ m_int[7]
 
 # Display received and calculated check bits:
 print(f'Received | Calculated check bits: ')
@@ -43,8 +44,8 @@ print(f'Cr0: {c_old[0]} | Cc0: {c0}\nCr1: {c_old[1]} | Cc1: {c1}\n'
       f'Cr2: {c_old[2]} | Cc2: {c2}\nCr3: {c_old[3]} | Cc3: {c3}\n')
 
 # Calculate, form and display error syndrome and its decimal equivalent
-error_syndrome = f'{c3 ^ c_old[3]}{c2 ^ c_old[2]}{c1 ^ c_old[1]}{c0 ^ c_old[0]}'
-error_syndrome_decimal = int(error_syndrome, 2)
+error_syndrome: str = f'{c3 ^ c_old[3]}{c2 ^ c_old[2]}{c1 ^ c_old[1]}{c0 ^ c_old[0]}'
+error_syndrome_decimal: int = int(error_syndrome, 2)
 print(f'Error syndrome: {error_syndrome} ({error_syndrome_decimal})')
 
 # Handle error syndrome
@@ -56,9 +57,9 @@ elif error_syndrome_decimal == 0:
 
     # Format and display infromation bits
     m = m[::-1]
-    m = ''.join(m)
-    m_formatted = m[:4] + " " + m[4:]
-    print(f'Original message was: {m_formatted} ({int(m, 2)})')
+    m_str = ''.join(m)
+    m_formatted = m_str[:4] + " " + m_str[4:]
+    print(f'Original message was: {m_formatted} ({int(m_str, 2)})')
 
 elif 0 < error_syndrome_decimal < 12:
     # Error syndrome 'points' at a check bit
@@ -68,9 +69,9 @@ elif 0 < error_syndrome_decimal < 12:
 
         # Format and display infromation bits
         m = m[::-1]
-        m = ''.join(m)
-        m_formatted = m[:4] + " " + m[4:]
-        print(f'Original message was: {m_formatted} ({int(m, 2)})')
+        m_str = ''.join(m)
+        m_formatted = m_str[:4] + " " + m_str[4:]
+        print(f'Original message was: {m_formatted} ({int(m_str, 2)})')
 
     # Error syndrome 'points' at information bit
     else:
@@ -84,16 +85,16 @@ elif 0 < error_syndrome_decimal < 12:
 
         # Format and display infromation bits
         m = m[::-1]
-        m = ''.join(m)
-        m_formatted = m[:4] + " " + m[4:]
-        print(f'Received message was {m_formatted} ({int(m, 2)}).')
+        m_str = ''.join(m)
+        m_formatted = m_str[:4] + " " + m_str[4:]
+        print(f'Received message was {m_formatted} ({int(m_str, 2)}).')
 
         m = [n_bin_cpy[2], n_bin_cpy[4], n_bin_cpy[5], n_bin_cpy[6],
              n_bin_cpy[8], n_bin_cpy[9], n_bin_cpy[10], n_bin_cpy[11]]
         m = m[::-1]
-        m = ''.join(m)
-        m_formatted = m[:4] + " " + m[4:]
-        print(f'Corrected message is: {m_formatted} ({int(m, 2)})')
+        m_str = ''.join(m)
+        m_formatted = m_str[:4] + " " + m_str[4:]
+        print(f'Corrected message is: {m_formatted} ({int(m_str, 2)})')
 
         print(f'Recevied code: {n_bin}')
         n_bin_cpy = n_bin_cpy[:4] + " " + n_bin_cpy[4:8] + " " + n_bin_cpy[8:12]
