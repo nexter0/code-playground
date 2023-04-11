3600aaced014d58cf13feca64f6565abae7b151a
Nikodem <166309@student.uwm.edu.pl>
2023-04-11 19:06:00 +0200

    reformatted file structure

--- hamming_code_calc/hamming_8bit_checker/main.py
+++ hamming_code_calc/hamming_8bit_checker/main.py
@@ -50,42 +50,51 @@ print(f'Error syndrome: {error_syndrome} ({error_syndrome_decimal})')
 # Handle error syndrome
 if error_syndrome_decimal > 11:
     print(f'Cannot resolve original message.')
+
 elif error_syndrome_decimal == 0:
     print('Found no errors in the code.')
+
     # Format and display infromation bits
     m = m[::-1]
     m = ''.join(m)
     m_formatted = m[:4] + " " + m[4:]
     print(f'Original message was: {m_formatted} ({int(m, 2)})')
+
 elif 0 < error_syndrome_decimal < 12:
     # Error syndrome 'points' at a check bit
     if error_syndrome_decimal in [1, 2, 4, 8]:
         print(f'Found an error in the code at position: {error_syndrome_decimal}.')
         print(f'This is a check bit that does not change information.')
+
         # Format and display infromation bits
         m = m[::-1]
         m = ''.join(m)
         m_formatted = m[:4] + " " + m[4:]
         print(f'Original message was: {m_formatted} ({int(m, 2)})')
+
     # Error syndrome 'points' at information bit
     else:
         print(f'Found an error in the code at position: {error_syndrome_decimal}.')
+
         # Correct the received code
         if int(n_bin_cpy[error_syndrome_decimal - 1]) == 0:
             n_bin_cpy = n_bin_cpy[:error_syndrome_decimal-1] + '1' + n_bin_cpy[error_syndrome_decimal:]
         else:
             n_bin_cpy = n_bin_cpy[:error_syndrome_decimal-1] + '0' + n_bin_cpy[error_syndrome_decimal:]
+
         # Format and display infromation bits
         m = m[::-1]
         m = ''.join(m)
         m_formatted = m[:4] + " " + m[4:]
         print(f'Received message was {m_formatted} ({int(m, 2)}).')
+
         m = [n_bin_cpy[2], n_bin_cpy[4], n_bin_cpy[5], n_bin_cpy[6],
-            n_bin_cpy[8], n_bin_cpy[9], n_bin_cpy[10], n_bin_cpy[11]]
+             n_bin_cpy[8], n_bin_cpy[9], n_bin_cpy[10], n_bin_cpy[11]]
         m = m[::-1]
         m = ''.join(m)
         m_formatted = m[:4] + " " + m[4:]
         print(f'Corrected message is: {m_formatted} ({int(m, 2)})')
+
         print(f'Recevied code: {n_bin}')
         n_bin_cpy = n_bin_cpy[:4] + " " + n_bin_cpy[4:8] + " " + n_bin_cpy[8:12]
         print(f'Corrected code: {n_bin_cpy}')
